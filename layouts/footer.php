60d519629d21fcdaa75860605c5fb27d502b38e1
Snehasis Sarkar <105881545+SnehasisSarkar@users.noreply.github.com>
2025-02-10 15:09:28 +0530

    Add files via upload

---
+++ layouts/footer.php
@@ -0,0 +1,89 @@
+    <!--Footer-->
+    <!-- <footer class="mt-5 py-5"> -->
+    <footer class="">
+      <div class="row container mx-auto mt-0 pt-4 pb-5">
+        <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+          <img class="logo2" src="assets/imgs/logo2.jpg" alt="" />
+          <p class="pt-3">
+            We provide the best products at the most affordable prices
+          </p>
+        </div>
+        <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+          <h5 class="pb-2">Featured</h5>
+          <ul class="text-uppercase">
+            <li><a href="#">men</a></li>
+            <li><a href="#">women</a></li>
+            <li><a href="#">boys</a></li>
+            <li><a href="#">girls</a></li>
+            <li><a href="#">new arrivals</a></li>
+            <li><a href="#">clothes</a></li>
+          </ul>
+        </div>
+
+        <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+          <h5 class="pb-2">Contact Us</h5>
+          <div>
+            <h6 class="text-uppercase">Address</h6>
+            <p>1234 Street Name, City</p>
+          </div>
+          <div>
+            <h6 class="text-uppercase">Phone</h6>
+            <p>123 456 7890</p>
+          </div>
+          <div>
+            <h6 class="text-uppercase">Email</h6>
+            <p>info@abc.com</p>
+          </div>
+        </div>
+
+        <div class="footer-one col-lg-3 col-md-6 col-sm-12">
+          <h5>Instagram</h5>
+          <div class="row pt-0 mt-0">
+            <img
+              src="assets/imgs/clothes1.jpg"
+              class="img-fluid w-25 h-25"
+              alt=""
+            />
+            <img
+              src="assets/imgs/clothes2.jpg"
+              class="img-fluid w-25 h-25"
+              alt=""
+            />
+            <img
+              src="assets/imgs/clothes3.jpg"
+              class="img-fluid w-25 h-25"
+              alt=""
+            />
+            <img
+              src="assets/imgs/clothes4.jpg"
+              class="img-fluid w-25 h-25"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+
+      <div class="copyright">
+        <div class="row container mx-auto px-0 pt-0 pb-4 mx-0 my-0">
+          <div class="col-lg-3 col-md-6 col-sm-12 mb-4">
+            <img src="assets/imgs/payment.jpg" alt="" />
+          </div>
+          <div class="col-lg-3 col-md-6 col-sm-12 mb-2 text-nowrap">
+            <p>Urban store &COPY; All Rights Reserved</p>
+          </div>
+          <div class="col-lg-3 col-md-6 col-sm-12 mb-4">
+            <a href="#"><i class="fab fa-facebook"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
